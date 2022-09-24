105dbc182dd54bc2b5d838ba22dda99e1f17900a
samgo23 <gor22001@byui.edu>
2022-09-24 16:25:54 -0600

    changes

---
+++ lesson2/scripts/script.js
@@ -0,0 +1,6 @@
+const date = new Date();
+let year = date.getFullYear();
+document.querySelector('#year').innerHTML = year;
+
+Time = document.lastModified;
+document.querySelector("#modify").innerHTML = document.lastModified;
--- scripts/script.js
+++ scripts/script.js
@@ -47,19 +47,5 @@ document.querySelector("#date2").textContent = fulldate;
 
 
 
-alert(document.lastModified);
-
-let oLastModif = new Date(document.lastModified);
-
-let nLastModif = Date.parse(document.lastModified);
-
-const lastVisit = parseFloat(document.cookie.replace(/(?:(?:^|.*;)\s*last_modif\s*=\s*([^;]*).*$)|^.*$/, "$1"));
-const lastModif = Date.parse(document.lastModified);
-
-if (isNaN(lastVisit) || lastModif > lastVisit) {
-document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${location.pathname}`;
-
-if (isFinite(lastVisit)) {
-    alert("This page has been changed!");
-}
-}
+Time = document.lastModified;
+document.querySelector("#modify").innerHTML = document.lastModified;
