9e2e58590a09416da7fc28ea7c5b1606bcb7703a
samgo23 <gor22001@byui.edu>
2022-10-15 13:44:58 -0600

    W05 changes

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -10,7 +10,7 @@ x.onclick = toggleMenu;
 
 
 /* current date and time on the top of the page */
-const dateTime = new Date();
+let dateTime = new Date();
 const fullDate = new Intl.DateTimeFormat('en-US', { dateStyle: 'long', timeStyle: 'short' }).format(dateTime);
 document.getElementById("dateTime").innerHTML = fullDate;
 /* current year used for copyright */
@@ -23,3 +23,18 @@ document.querySelector('#year').innerHTML = year;
 Time = document.lastModified;
 document.querySelector("#modify").innerHTML = document.lastModified;
 
+const meetingDay = new Date();
+const monTues = dateTime.getDate();
+
+function isMonTues(day) {
+    if (day === 1){
+        return true;
+    }
+    else if (day === 2) {
+        return true;
+    }
+    else {
+        return false; 
+    } 
+} 
+document.querySelector(".meeting").classList.add("closed"); 
